a08ea807a6675a5cf13475dcf8a6a6836e6bb008
Chivon <chhaichivon@2ntkh.com>
2017-07-21 06:02:22 +0700

    Init project

---
+++ src/main/java/com/wordpress/chhaichivon/controllers/AppController.java
@@ -0,0 +1,17 @@
+package com.wordpress.chhaichivon.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppController {
+	
+	
+	@RequestMapping(value ="/", method = RequestMethod.GET)
+	public String index(){
+		System.out.println("Home Page");
+		return "index";
+	}
+
+}
